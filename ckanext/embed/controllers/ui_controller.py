5ba9307bfb89c74d87a0d456a63c2fa88fff7852
Robley Gori <robleyadrian@gmail.com>
2017-11-06 11:23:15 +0300

    [Feature] Add controller to display a single dataset

--- ckanext/embed/controllers/ui_controller.py
+++ ckanext/embed/controllers/ui_controller.py
@@ -2,20 +2,20 @@
 
 # Copyright (c) 2015-2016 CoNWeT Lab., Universidad Politécnica de Madrid
 
-# This file is part of CKAN Data Requests Extension.
+# This file is part of CKAN Embed Extension.
 
-# CKAN Data Requests Extension is free software: you can redistribute it and/or
+# CKAN Embed Extension is free software: you can redistribute it and/or
 # modify it under the terms of the GNU Affero General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 
-# CKAN Data Requests Extension is distributed in the hope that it will be useful,
+# CKAN Embed Extension is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Affero General Public License for more details.
 
 # You should have received a copy of the GNU Affero General Public License
-# along with CKAN Data Requests Extension. If not, see <http://www.gnu.org/licenses/>.
+# along with CKAN Embed Extension. If not, see <http://www.gnu.org/licenses/>.
 
 import logging
 
@@ -24,11 +24,9 @@ import ckan.lib.helpers as h
 import ckan.model as model
 import ckan.plugins as plugins
 import ckanext.embed.constants as constants
-import functools
 import re
 
 from ckan.common import request, _, g, c
-from urllib import urlencode
 
 
 _link = re.compile(r'(?:(https?://)|(www\.))(\S+\b/?)([!"#$%&\'()*+,\-./:;<=>?@[\\\]^_`{|}~]*)(\s|$)', re.I)
@@ -42,12 +40,10 @@ class EmbedUI(base.BaseController):
 
     def index(self):
         # package search
-        context = {'model': model, 'session': model.Session,
-                    'user': c.user, 'auth_user_obj': c.userobj}
+        context = {'model': model, 'session': model.Session}
         organization = str(request.GET.get('organization', None))
         q = request.GET.get('q', '*:*')
-        print(organization)
-        print(q)
+
         data_dict = {
             'q': q,
             'facet.field': g.facets,
@@ -60,3 +56,17 @@ class EmbedUI(base.BaseController):
         results = tk.get_action(constants.EMBED_INDEX)(context, data_dict)
 
         return base.render('embed/index.html', extra_vars={'datasets': results.datasets}, cache_force=True)
+
+    def show(self, id):
+        data_dict = {'id': id}
+        context = {'model': model, 'session': model.Session}
+
+        if not data_dict['id']:
+            raise tk.ValidationError(tk._('Dataset ID has not been included'))
+
+        # Get the dataset
+        result = tk.get_action(constants.EMBED_SHOW)(context, data_dict)
+        if not result:
+            raise tk.ObjectNotFound(tk._('Dataset %s not found in the data base') % id)
+
+        return base.render('embed/show.html', extra_vars={'dataset': result}, cache_force=True)
