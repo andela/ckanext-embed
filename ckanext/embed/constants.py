60946b085e97c753c9810215065d74f5dd1e9e0e
Robley Gori <robleyadrian@gmail.com>
2017-11-06 11:22:40 +0300

    [Feature] Add the EMBED_SHOW constant

--- ckanext/embed/constants.py
+++ ckanext/embed/constants.py
@@ -2,20 +2,21 @@
 
 # Copyright (c) 2015 CoNWeT Lab., Universidad Politécnica de Madrid
 
-# This file is part of CKAN Data Requests Extension.
+# This file is part of CKAN Embed Extension.
 
-# CKAN Data Requests Extension is free software: you can redistribute it and/or
+# CKAN Embed Extension is free software: you can redistribute it and/or
 # modify it under the terms of the GNU Affero General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 
-# CKAN Data Requests Extension is distributed in the hope that it will be useful,
+# CKAN Embed Extension is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Affero General Public License for more details.
 
 # You should have received a copy of the GNU Affero General Public License
-# along with CKAN Data Requests Extension. If not, see <http://www.gnu.org/licenses/>.
+# along with CKAN Embed Extension. If not, see <http://www.gnu.org/licenses/>.
 
 EMBED_MAIN_PATH = 'embed'
 EMBED_INDEX = 'embed_index'
+EMBED_SHOW = 'embed_show'
